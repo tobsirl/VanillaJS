b9d8db3b1cfd65eba4c41d738e07ee424d29e5bb
tobsirl <tobster111@gmail.com>
2020-02-21 16:38:35 +0000

    Checking for a valid email

--- FormValidator/js/script.js
+++ FormValidator/js/script.js
@@ -18,6 +18,12 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 
+// Check email is valid
+function isValidateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
@@ -26,5 +32,13 @@ form.addEventListener('submit', function(e) {
     showError(username, `Username is required`);
   } else {
     showSuccess(username);
+  }
+
+  if (email.value === '') {
+    showError(email, `Email is required`);
+  } else if (!isValidateEmail(email.value)) {
+    showError(email, `Email is not valid`);
+  } else {
+    showSuccess(email);
   }
 });
