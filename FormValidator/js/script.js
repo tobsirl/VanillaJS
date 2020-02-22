3e2de819db1051af4a281d08d48fa1c6b79500b8
tobsirl <tobster111@gmail.com>
2020-02-22 15:41:36 +0000

    Checking input length

--- FormValidator/js/script.js
+++ FormValidator/js/script.js
@@ -24,6 +24,23 @@ function isValidateEmail(email) {
   return re.test(String(email).toLowerCase());
 }
 
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length <= min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
 // Uppercase on the first letter
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -45,4 +62,6 @@ form.addEventListener('submit', function(e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
 });
