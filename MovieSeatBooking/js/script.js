c17de7add9187578758ca624e2da041a599371c0
tobsirl <tobster111@gmail.com>
2020-02-29 11:14:54 +0000

    Pull in all the elements from the document

---
+++ MovieSeatBooking/js/script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+const ticketPrice = +movieSelect.value;
+
+console.log(ticketPrice);
