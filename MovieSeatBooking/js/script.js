159f9e28e2994a62a5a741f015bc07c522b72711
tobsirl <tobster111@gmail.com>
2020-03-01 09:51:47 +0000

    Add an eventlistener to the seats, get the number of seats selected and the total amount

--- MovieSeatBooking/js/script.js
+++ MovieSeatBooking/js/script.js
@@ -6,4 +6,20 @@ const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value;
 
-console.log(ticketPrice);
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  console.log(selectedSeats);
+}
+
+container.addEventListener('click', event => {
+  if (
+    event.target.classList.contains('seat') &&
+    !event.target.classList.contains('occupied')
+  ) {
+    event.target.classList.toggle('selected');
+  }
+
+  updateSelectedCount();
+});
